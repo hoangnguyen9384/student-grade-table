a352a00c9f5a99d41e841a366a66309058eb5439
hoangnguyen9384 <hoangnguyen9384@gmail.com>
2020-08-22 11:46:55 -0700

    Add a Pageheader component

---
+++ components/page-header.js
@@ -0,0 +1,9 @@
+class PageHeader {
+  constructor(headerElelment){
+    this.headerElelment = headerElelment;
+  }
+  updateAverage(newAverage){
+    console.log(newAverage);
+
+  }
+}
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+var headerElement = document.querySelector("header");
+var newPageHeader = new PageHeader(headerElement);
 var tableElement = document.querySelector("table");
 var gradeTable = new GradeTable(tableElement);
-var app = new App(gradeTable);
+var app = new App(gradeTable, PageHeader);
 app.start();
