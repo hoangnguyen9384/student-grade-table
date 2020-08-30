f441a9c2e698f025e4981ae67082c8d13ba9092a
hoangnguyen9384 <hoangnguyen9384@gmail.com>
2020-08-29 20:36:08 -0700

    updated

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var headerElement = document.querySelector("header");
 var newPageHeader = new PageHeader(headerElement);
 var tableElement = document.querySelector("table");
 
-var p = document.querySelector("p");
+var p = document.getElementById("hidden-p");
 var gradeTable = new GradeTable(tableElement, p);
 
 var app = new App(gradeTable, newPageHeader, newGradeForm);
